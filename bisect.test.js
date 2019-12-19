83a5d51291b5dfcc88465db8da20647e2042576b
Filip Haglund <filip.l.haglund@gmail.com>
2019-12-19 10:11:27 +0100

    :white_check_mark: Add tests for 100% coverage

--- bisect.test.js
+++ bisect.test.js
@@ -1,9 +1,32 @@
 const {bisect, polyfill} = require('./bisect.js');
 
-test('Bisect numbers into evens and odds', () => expect(bisect([1, 2, 3, 4, 5, 6, 7, 8, 9], n => n % 2)).toStrictEqual([[1, 3, 5, 7, 9], [2, 4, 6, 8]]));
+const evenOddPredicate = n => n % 2;
+const digits = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const bisectedDigits = [[1, 3, 5, 7, 9], [2, 4, 6, 8]];
+
+test('Bisect numbers into evens and odds', () =>
+  expect(bisect(digits, evenOddPredicate)).toStrictEqual(bisectedDigits));
+
+test('Supplying non-array as first argument should throw a TypeError', () => {
+  expect(() => {
+    bisect('This is not an array', evenOddPredicate)
+  }).toThrowError(TypeError);
+});
+
+test('Supplying non-function as second argument should throw a TypeError', () => {
+  expect(() => {
+    bisect(['This is  an array'])
+  }).toThrowError(TypeError);
+});
 
 test('Apply polyfill and bisect into evens and odds', () => {
   polyfill();
 
-  expect([1, 2, 3, 4, 5, 6, 7, 8, 9].bisect(n => n % 2)).toStrictEqual([[1, 3, 5, 7, 9], [2, 4, 6, 8]]);
+  expect(digits.bisect(n => n % 2)).toStrictEqual(bisectedDigits);
+});
+
+test('Polyfilling a second time breaks nothing', () => {
+  polyfill();
+
+  expect(digits.bisect(n => n % 2)).toStrictEqual(bisectedDigits);
 });
